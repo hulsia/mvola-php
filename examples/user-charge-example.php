08996d4933b45cf1b2ee1cbb3386c9c3ef53d3d9
Nekena Ratafita <nekena-tanaloua@users.noreply.github.com>
2022-05-10 01:59:46 +0300

    update user charge example

--- examples/user-charge-example.php
+++ examples/user-charge-example.php
@@ -1,20 +1,31 @@
 <?php
 
+include '../vendor/autoload.php';
+
 /**
  * @see https://www.mvola.mg/devportal/home to get your credentials
+ * @return \Hulsia\MVola\MVola mvola API
  */
 $mvola = new \Hulsia\MVola\MVola([
     'consumerKey' => 'YOUR_CONSUMER_KEY',
     'consumerSecret' => 'YOUR_CONSUMER_SECRET',
-    'live' => true, // by default, the library will use the sandbox environment
+    'live' => false, // by default, the library will use the sandbox environment
 ]);
 
+/**
+ * Configuration of the merchant pay API
+ * @return \Hulsia\MVola\MerchantPay merchant API instance
+ */
 $merchantPay = new \Hulsia\MVola\MerchantPay($mvola, [
     'UserLanguage' => 'MG',
     'UserAccountIdentifier' => 'msisdn;0343500004',
     'partnerName' => 'MVola',
 ]);
 
+/**
+ * Example of transaction charge with the merchant pay API
+ * @return array The response from the MVola API
+ */
 $transtation = $merchantPay->initTransaction([
     'amount' => '10000',
     'currency' => 'Ar', // currency must be 'Ar'
